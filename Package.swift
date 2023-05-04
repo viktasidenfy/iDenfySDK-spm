cdbf70a37dba9921c34d28d5a9dc0463f8c30d2d
viktasidenfy <78145743+viktasidenfy@users.noreply.github.com>
2023-05-04 12:25:36 +0300

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,109 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version = "7.8.1"
+
+let package = Package(
+    name: "iDenfySDK",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(
+            name: "iDenfySDK-Dynamic",
+            type: .dynamic,
+            targets: ["iDenfySDKTarget"]),
+        .library(
+            name: "iDenfySDK",
+            targets: ["iDenfySDKTarget"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/airbnb/lottie-spm.git", "4.2.0"..<"4.2.1"),
+    ],
+    targets: [
+        //IdenfyViews
+        .target(
+            name: "idenfyviewsTarget",
+            dependencies: [.target(name: "idenfyviewsWrapper",
+                                   condition: .when(platforms: [.iOS]))],
+            path: "SwiftPM-PlatformExclude/idenfyviewsWrap"
+        ),
+        .target(
+            name: "idenfyviewsWrapper",
+            dependencies: [
+                .target(
+                    name: "idenfyviews",
+                    condition: .when(platforms: [.iOS])
+                ),
+                .target(name: "idenfycore",
+                        condition: .when(platforms: [.iOS])),
+                .product(name: "Lottie",
+                         package: "lottie-spm",
+                         condition: .when(platforms: [.iOS])),
+            ],
+            path: "idenfyviewsWrapper"
+        ),
+        //IdenfyNFCReading
+        .target(
+            name: "idenfyNFCReadingTarget",
+            dependencies: [.target(name: "idenfyNFCReadingWrapper",
+                                   condition: .when(platforms: [.iOS]))],
+            path: "SwiftPM-PlatformExclude/idenfyNFCReadingWrap"
+        ),
+        .target(
+            name: "idenfyNFCReadingWrapper",
+            dependencies: [
+                .target(
+                    name: "idenfyNFCReading",
+                    condition: .when(platforms: [.iOS])
+                ),
+                .target(name: "OpenSSL",
+                        condition: .when(platforms: [.iOS])),
+            ],
+            path: "idenfyNFCReadingWrapper"
+        ),
+        //iDenfySDK
+        .target(
+            name: "iDenfySDKTarget",
+            dependencies: [.target(name: "iDenfySDKWrapper",
+                                   condition: .when(platforms: [.iOS]))],
+            path: "SwiftPM-PlatformExclude/iDenfySDKWrap",
+            cSettings: [
+                .define("CLANG_MODULES_AUTOLINK", to: "YES")
+            ]
+        ),
+        .target(
+            name: "iDenfySDKWrapper",
+            dependencies: [
+                .target(
+                    name: "iDenfySDK",
+                    condition: .when(platforms: [.iOS])),
+                .product(name: "Lottie",
+                         package: "lottie-spm",
+                         condition: .when(platforms: [.iOS])),
+                .target(name: "idenfycore",
+                        condition: .when(platforms: [.iOS])),
+                .target(name: "iDenfyInternalLogger",
+                        condition: .when(platforms: [.iOS])),
+                .target(name: "idenfyNFCReadingTarget",
+                        condition: .when(platforms: [.iOS])),
+                .target(name: "idenfyviewsTarget",
+                        condition: .when(platforms: [.iOS])),
+            ],
+            path: "iDenfySDKWrapper"
+        ),
+        // Binaries
+        .binaryTarget(name: "iDenfyInternalLogger",
+                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfySDK/iDenfyInternalLogger.zip", checksum: "2bb850c786e572beebfde9c338618f6d48fcfa4e4146203fd89eb70983c0701d"),
+        .binaryTarget(name: "idenfyviews",
+                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfySDK/idenfyviews.zip", checksum: "700806b992fbcc1d4330720cc4c456dc0c5ff6ebc39da059ddcbd70e038a5d98"),
+        .binaryTarget(name: "iDenfySDK",
+                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfySDK/iDenfySDK.zip", checksum: "a0fb542234e9a4d7dd047e71d1d68308789647616ba8812979820164de15eab7"),
+        .binaryTarget(name: "idenfycore",
+                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfySDK/idenfycore.zip", checksum: "ad24d7b035746206d350acb490a737ef283c66f0bc1f307628f70c4e948618d9"),
+        .binaryTarget(name: "idenfyNFCReading",
+                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfySDK/idenfyNFCReading.zip", checksum: "206955bf575e39e0b7d696ccf9b83ac65c33a830aabc0d1b499058b1b902530f"),
+        .binaryTarget(name: "OpenSSL",
+                      url: "https://s3.eu-west-1.amazonaws.com/sdk.builds/ios-sdk/\(version)/spm/IdenfySDK/OpenSSL.zip", checksum: "d7caf5d42da4cd6bb3a21e0187d10a2b6d8b982a72b71246d95020a12f2ef342"),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# iDenfySDK pacakge for SPM
+
+Full documentation of iDenfy SDK: https://documentation.idenfy.com/mobile/iOS/ios-sdk
