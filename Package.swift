2f48264b0a5be053d4a51349760be77227e4ed6c
Bitrise CI <noreply@bitrise.io>
2026-03-04 10:24:55 +0000

    new version

--- Package.swift
+++ Package.swift
@@ -3,13 +3,13 @@
 
 import PackageDescription
 
-let version = "8.7.9"
+let version = "8.8.0"
 
 enum Checksums {
-    static let iDenfyInternalLoggerChecksum = "6c459c26b580972f329c24984d4f62cba20d1f81d0acd523b8c17e37d9508c3f"
-    static let idenfyviewsChecksum = "6971d1bb6d0056ebc54276e7625803844745971aaa680acd6977e9b440854175"
-    static let iDenfySDKChecksum = "08b70c2b268c2d9ff33d45f6a8d5bff6444d9b05d470a42636832be5cea33b35"
-    static let idenfycoreChecksum = "3f260a5e08f88709c90227b9cb540e44fd94823153a4d08a1aa754f9b608ef4b"
+    static let iDenfyInternalLoggerChecksum = "b14a9c385ca2a81b7e0a942337ba2aa076d980789cccf6186944c5b53ddd1c2f"
+    static let idenfyviewsChecksum = "fe5600b4398adff20db986b5cde6cca8c11e6ebf485f7ef2a486ad6a6f186fb9"
+    static let iDenfySDKChecksum = "49d04bd5a35dd8b92dbad0b7d16a3eed0a338d15b121ec3f17a59a428ec9b45f"
+    static let idenfycoreChecksum = "c5c00c9ae295de77d14ded56f70d5ba0b4f59f7c1bdadc46a5e781e592aea8e1"
 }
 
 let package = Package(
